31edaa3cd1d4d986aff5a37554c45d7770330a3a
Schude <nemli.serkan2@gmail.com>
2021-05-27 02:38:18 +0300

    Destructuring props

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,30 +3,34 @@ import "./Card.css";
 import heartOutline from "../../assets/heart-outline.png"; // Tell webpack this JS file uses this image
 import heartFill from "../../assets/heart-fill.png"; // Tell webpack this JS file uses this image
 
-// import food from "../../assets/food.jpg"; // Image come as prop
+// import food from "../../assets/food.jpg"; // Image passed as prop
 
 export default function Card(props) {
+
+  // Destructuring props
+  const {recipeItem,author,likeCount,isLiked} = props
+
   return (
     <div className="card">
       <div className="card-header">
         <div className="profile">
-          <span className="letter">{props.author[0]}</span>
+          <span className="letter">{author[0]}</span>
         </div>
         <div className="card-title-group">
-          <h5 className="card-title"> {props.recipeItem.title} </h5>
-          <div className="card-date">{props.recipeItem.date}</div>
+          <h5 className="card-title"> {recipeItem.title} </h5>
+          <div className="card-date">{recipeItem.date}</div>
         </div>
       </div>
-      <img className="card-image" src={props.recipeItem.image} alt="Logo" />
-      <div className="card-text">{props.recipeItem.description}</div>
+      <img className="card-image" src={recipeItem.image} alt="Logo" />
+      <div className="card-text">{recipeItem.description}</div>
       <div className="card-like-bar">
-        {props.isLiked ? (
+        {isLiked ? (
           <img className="card-like-icon" src={heartFill} alt="Logo" />
         ) : (
           <img className="card-like-icon" src={heartOutline} alt="Logo" />
         )}
         <div className="like-text">
-          <b> {props.likeCount} </b> kişi bu tarifi beğendi.
+          <b> {likeCount} </b> kişi bu tarifi beğendi.
         </div>
       </div>
     </div>
